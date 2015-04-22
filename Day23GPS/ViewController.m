d5841225aaa46a33ad16142d1f09981d8216387d
WANG ACE <wlop161op@qq.com>
2015-04-22 17:52:09 +0800

    GPS
    GPS

--- Day23GPS/ViewController.m
+++ Day23GPS/ViewController.m
@@ -7,16 +7,47 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
-
+#import <CoreLocation/CoreLocation.h>
+@interface ViewController ()<CLLocationManagerDelegate>
+@property (nonatomic, strong)CLLocationManager *manager;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+//    询问是否开启了位置服务
+    if ([CLLocationManager locationServicesEnabled]) {
+       self.manager = [[CLLocationManager alloc]init];
+//        设置精度
+        [self.manager setDesiredAccuracy:kCLLocationAccuracyBest];
+//       设置更新距离 距离越小越精准 越费电
+        [self.manager setDistanceFilter:20];
+        self.manager.delegate = self;
+        //开始更新经纬度
+        [self.manager startUpdatingLocation];
+        //获取方向 陀螺仪的值
+        [self.manager startUpdatingHeading];
+        
+        
+    }
+}
+
+
+- (void)locationManager:(CLLocationManager *)manager
+    didUpdateToLocation:(CLLocation *)newLocation
+           fromLocation:(CLLocation *)oldLocation{
+    CLLocationCoordinate2D coord = newLocation.coordinate;
+    
+    NSLog(@"经度：%lf  维度：%lf",coord.longitude,coord.latitude);
+    
+}
+
+
+//获取陀螺仪的值
+-(void)locationManager:(CLLocationManager *)manager didUpdateHeading:(CLHeading *)newHeading{
+    NSLog(@"角度：%f",newHeading.trueHeading);
 }
 
 - (void)didReceiveMemoryWarning {
